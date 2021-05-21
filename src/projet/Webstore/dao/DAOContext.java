42b3176c715b8ec6af32080b842d8cb7764df4dd
brulej <brule.jeremie@gmail.com>
2021-05-21 21:14:18 +0100

    ajout fichier properties

--- src/projet/Webstore/dao/DAOContext.java
+++ src/projet/Webstore/dao/DAOContext.java
@@ -1,14 +1,20 @@
 package projet.Webstore.dao;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletContext;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-public class DAOContext {
+public class DAOContext  {
     
     protected static String dbURL= "toto";
     protected static String dbLogin;
@@ -17,12 +23,19 @@ public class DAOContext {
     
     public static void setconnexion() {
     	System.out.println("init");
-        try {
-            
-            Class.forName( "com.mysql.jdbc.Driver" ) ;
-            dbURL = "jdbc:mysql://localhost:3306/webstore" ;
-            dbLogin =  "root" ;
-            dbPassword =  "" ;
+    	
+    	Properties props = new Properties();
+		try (FileInputStream fis = new FileInputStream("C:\\Users\\jer91\\OneDrive\\Bureau\\projet_web\\src\\projet\\Webstore\\dao\\config.properties")){
+			props.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+        try { 
+            Class.forName( props.getProperty("jdbc.driver.class" )) ;
+            dbURL = props.getProperty("jdbc.url") ;
+            dbLogin =  props.getProperty("jdbc.login");
+            dbPassword =  props.getProperty("jdbc.password") ;
             
         } catch( Exception exception ) {
            System.out.println("je suis ici");  
