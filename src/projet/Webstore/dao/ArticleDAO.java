0267528b47af7962d7caa746d520f1ff1c6148ac
brulej <brule.jeremie@gmail.com>
2021-05-26 12:24:44 +0100

    reparation

--- src/projet/Webstore/dao/ArticleDAO.java
+++ src/projet/Webstore/dao/ArticleDAO.java
@@ -15,8 +15,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
-
 public class ArticleDAO extends DAOContext {
 
     public static int getArticleCount() {
